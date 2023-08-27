09858f878b0a1a620e3f300ff4ddaefa5366fdea
Spandan Saxena <spandansaxena@gmail.com>
2023-08-28 00:31:45 +0530

    One Button to Work

--- app/src/main/java/com/onesilicondiode/batterywise/MainActivity.java
+++ app/src/main/java/com/onesilicondiode/batterywise/MainActivity.java
@@ -1,9 +1,8 @@
 package com.onesilicondiode.batterywise;
 
-import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,26 +10,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-    MaterialButton startSaving;
+    MaterialButton toggleService;
+    private static final String PREFS_NAME = "MyPrefsFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // Initialize the MediaPlayer
-        startSaving = findViewById(R.id.saveBatteryBtn);
-        startSaving.setOnClickListener(view -> {
-            Intent serviceIntent = new Intent(this, BatteryMonitorService.class);
-            startService(serviceIntent);
-            startSaving.setText(R.string.enabled);
-            startSaving.setEnabled(false);
-            Toast.makeText(MainActivity.this, "Service Enabled", Toast.LENGTH_SHORT).show();
-            finish();
+        toggleService = findViewById(R.id.saveBatteryBtn);
+        // Check if the service is running and set button text accordingly
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean isServiceRunning = prefs.getBoolean("isServiceRunning", false);
+
+        if (isServiceRunning) {
+            toggleService.setText(R.string.enable);
+        } else {
+            toggleService.setText(R.string.disable);
+        }
+
+        toggleService.setOnClickListener(view -> {
+            if (isServiceRunning) {
+                // Stop the service
+                Intent serviceIntent = new Intent(this, BatteryMonitorService.class);
+                stopService(serviceIntent);
+                toggleService.setText(R.string.enable);
+                Toast.makeText(MainActivity.this, "Service Stopped", Toast.LENGTH_SHORT).show();
+            } else {
+                // Start the service
+                Intent serviceIntent = new Intent(this, BatteryMonitorService.class);
+                startService(serviceIntent);
+                toggleService.setText(R.string.disable);
+                Toast.makeText(MainActivity.this, "Service Enabled", Toast.LENGTH_SHORT).show();
+            }
+
+            // Toggle the service running state in shared preferences
+            SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+            editor.putBoolean("isServiceRunning", !isServiceRunning);
+            editor.apply();
         });
-    }
-    public void stopMonitoring(View view) {
-        Intent serviceIntent = new Intent(this, BatteryMonitorService.class);
-        stopService(serviceIntent);
     }
 }
