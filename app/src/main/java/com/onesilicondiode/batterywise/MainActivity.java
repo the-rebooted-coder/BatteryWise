3f9c0ef7aeb8c9519a797f28de84969221bea86e
Spandan Saxena <spandansaxena@gmail.com>
2023-08-28 00:39:40 +0530

    Works

--- app/src/main/java/com/onesilicondiode/batterywise/MainActivity.java
+++ app/src/main/java/com/onesilicondiode/batterywise/MainActivity.java
@@ -1,53 +1,74 @@
 package com.onesilicondiode.batterywise;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    MaterialButton toggleService;
+    MaterialButton startSaving, stopSaving;
+    private boolean isServiceRunning = false;
     private static final String PREFS_NAME = "MyPrefsFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         // Initialize the MediaPlayer
-        toggleService = findViewById(R.id.saveBatteryBtn);
-        // Check if the service is running and set button text accordingly
+        startSaving = findViewById(R.id.saveBatteryBtn);
+        stopSaving = findViewById(R.id.closeBatteryBtn);
+
+        // Check if the service is running and update button visibility
         SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-        boolean isServiceRunning = prefs.getBoolean("isServiceRunning", false);
+        isServiceRunning = prefs.getBoolean("isServiceRunning", false);
 
         if (isServiceRunning) {
-            toggleService.setText(R.string.enable);
+            startSaving.setVisibility(View.INVISIBLE);
+            stopSaving.setVisibility(View.VISIBLE);
         } else {
-            toggleService.setText(R.string.disable);
+            startSaving.setVisibility(View.VISIBLE);
+            stopSaving.setVisibility(View.INVISIBLE);
         }
 
-        toggleService.setOnClickListener(view -> {
-            if (isServiceRunning) {
-                // Stop the service
-                Intent serviceIntent = new Intent(this, BatteryMonitorService.class);
-                stopService(serviceIntent);
-                toggleService.setText(R.string.enable);
-                Toast.makeText(MainActivity.this, "Service Stopped", Toast.LENGTH_SHORT).show();
-            } else {
-                // Start the service
-                Intent serviceIntent = new Intent(this, BatteryMonitorService.class);
-                startService(serviceIntent);
-                toggleService.setText(R.string.disable);
-                Toast.makeText(MainActivity.this, "Service Enabled", Toast.LENGTH_SHORT).show();
-            }
+        startSaving.setOnClickListener(view -> {
+            Intent serviceIntent = new Intent(this, BatteryMonitorService.class);
+            startService(serviceIntent);
+            startSaving.setVisibility(View.INVISIBLE);
+            stopSaving.setVisibility(View.VISIBLE);
+            Toast.makeText(MainActivity.this, "Service Enabled", Toast.LENGTH_SHORT).show();
 
-            // Toggle the service running state in shared preferences
+            // Update shared preferences
             SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
-            editor.putBoolean("isServiceRunning", !isServiceRunning);
+            editor.putBoolean("isServiceRunning", true);
             editor.apply();
+            finish();
+        });
+
+        stopSaving.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent serviceIntent = new Intent(MainActivity.this, BatteryMonitorService.class);
+                stopService(serviceIntent);
+                Toast.makeText(MainActivity.this, "Service Stopped", Toast.LENGTH_SHORT).show();
+                stopSaving.setVisibility(View.INVISIBLE);
+                startSaving.setVisibility(View.VISIBLE);
+
+                // Update shared preferences
+                SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+                editor.putBoolean("isServiceRunning", false);
+                editor.apply();
+                finish();
+            }
         });
     }
 }
