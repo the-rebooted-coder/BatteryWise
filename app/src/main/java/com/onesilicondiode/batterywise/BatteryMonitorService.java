d50ddb35cdbafe979135ea59a7aba9408f2412ea
Spandan Saxena <spandansaxena@gmail.com>
2023-08-28 00:12:32 +0530

    Notification Added

--- app/src/main/java/com/onesilicondiode/batterywise/BatteryMonitorService.java
+++ app/src/main/java/com/onesilicondiode/batterywise/BatteryMonitorService.java
@@ -1,5 +1,8 @@
 package com.onesilicondiode.batterywise;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -7,7 +10,12 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.media.MediaPlayer;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
 import com.onesilicondiode.batterywise.R;
 
@@ -15,6 +23,9 @@ public class BatteryMonitorService extends Service {
     private MediaPlayer mediaPlayer;
     private BroadcastReceiver batteryReceiver;
     private boolean alertPlayed = false;
+
+    private static final int FOREGROUND_SERVICE_ID = 101;
+    private static final String CHANNEL_ID = "BatteryMonitorChannel";
 
     @Override
     public void onCreate() {
@@ -44,11 +55,32 @@ public class BatteryMonitorService extends Service {
         // Register the BroadcastReceiver with the intent filter
         IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
         registerReceiver(batteryReceiver, intentFilter);
+
+        // Create a notification channel (required for Android 8.0 and above)
+        NotificationChannel channel = new NotificationChannel(
+                CHANNEL_ID,
+                "Battery Monitor",
+                NotificationManager.IMPORTANCE_HIGH);
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+
+        // Create a notification for the foreground service
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("SafeCharge")
+                .setContentText("Battery levels are being monitored.")
+                .setSmallIcon(R.drawable.ic_notification)
+                .setColor(ContextCompat.getColor(this, R.color.green))
+                .setColorized(true)
+                .build();
+
+        // Start the service as a foreground service
+        startForeground(FOREGROUND_SERVICE_ID, notification);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        // Service should continue running until explicitly stopped
+        // Service should continue running and restart if killed
         return START_STICKY;
     }
 
--- app/src/main/java/com/onesilicondiode/batterywise/MainActivity.java
+++ app/src/main/java/com/onesilicondiode/batterywise/MainActivity.java
@@ -3,6 +3,7 @@ package com.onesilicondiode.batterywise;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,11 +21,16 @@ public class MainActivity extends AppCompatActivity {
         // Initialize the MediaPlayer
         startSaving = findViewById(R.id.saveBatteryBtn);
         startSaving.setOnClickListener(view -> {
-            Intent serviceIntent = new Intent(MainActivity.this, BatteryMonitorService.class);
+            Intent serviceIntent = new Intent(this, BatteryMonitorService.class);
             startService(serviceIntent);
             startSaving.setText(R.string.enabled);
             startSaving.setEnabled(false);
             Toast.makeText(MainActivity.this, "Service Enabled", Toast.LENGTH_SHORT).show();
+            finish();
         });
+    }
+    public void stopMonitoring(View view) {
+        Intent serviceIntent = new Intent(this, BatteryMonitorService.class);
+        stopService(serviceIntent);
     }
 }
