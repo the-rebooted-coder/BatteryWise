356e4a4f91826446c04cff733b2a23584f92f0f1
Spandan Saxena <spandansaxena@gmail.com>
2023-08-28 15:14:35 +0530

    Service Auto Starts When Booted

---
+++ app/src/main/java/com/onesilicondiode/batterywise/BootReceiver.java
@@ -0,0 +1,16 @@
+package com.onesilicondiode.batterywise;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Check if the device has finished booting
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent serviceIntent = new Intent(context, BatteryMonitorService.class);
+            context.startService(serviceIntent);
+        }
+    }
+}
