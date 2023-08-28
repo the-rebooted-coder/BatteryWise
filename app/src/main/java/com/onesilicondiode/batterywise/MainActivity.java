7e2a923f861f550cfb668d013be92b51ef9966b6
Spandan Saxena <spandansaxena@gmail.com>
2023-08-28 11:56:37 +0530

    New notify tone

--- app/src/main/java/com/onesilicondiode/batterywise/MainActivity.java
+++ app/src/main/java/com/onesilicondiode/batterywise/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
 
         startSaving.setOnClickListener(view -> {
             Intent serviceIntent = new Intent(this, BatteryMonitorService.class);
-            serviceIntent.putExtra("isUserStarted", true);
             startService(serviceIntent);
             startSaving.setVisibility(View.INVISIBLE);
             stopSaving.setVisibility(View.VISIBLE);
@@ -63,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
                 SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
                 editor.putBoolean("isServiceRunning", false);
                 editor.apply();
-                finish();
             }
         });
     }
