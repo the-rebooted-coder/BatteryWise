fc748d221f3eae7eec1fe5574b6b22582a5a9cc9
Spandan Saxena <spandansaxena@gmail.com>
2023-08-31 23:56:05 +0530

    Light View Created

--- app/src/main/java/com/onesilicondiode/batterywise/Second_Startup.java
+++ app/src/main/java/com/onesilicondiode/batterywise/Second_Startup.java
@@ -5,7 +5,12 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
@@ -13,6 +18,8 @@ import androidx.core.splashscreen.SplashScreen;
 import com.google.android.material.color.DynamicColors;
 
 public class Second_Startup extends AppCompatActivity {
+    private TextView moreProductInfo, learnMore;
+
 
     public static int getThemeColor(Context context, int colorResId) {
         TypedValue typedValue = new TypedValue();
@@ -21,12 +28,14 @@ public class Second_Startup extends AppCompatActivity {
         typedArray.recycle();
         return color;
     }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         // Specify a custom return transition animation
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.installSplashScreen(this);
@@ -36,7 +45,24 @@ public class Second_Startup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setEnterTransition(null);
         setContentView(R.layout.activity_second_startup);
+        moreProductInfo = findViewById(R.id.moreProductInfo);
+        learnMore = findViewById(R.id.learnMore);
+        learnMore.setOnClickListener(v -> {
+            // Fade in the "moreProductInfo" TextView
+            fadeInTextView(moreProductInfo);
+        });
         ImageView sharedImageView = findViewById(R.id.sharedImageView);
         sharedImageView.setTransitionName("imageTransition");
+        TextView wavingHandTextView = findViewById(R.id.wavingHandTextView);
+        Animation waveAnimation = AnimationUtils.loadAnimation(this, R.anim.emoji_wave);
+        wavingHandTextView.startAnimation(waveAnimation);
+    }
+
+    private void fadeInTextView(TextView textView) {
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setDuration(1000); // Set the duration of the fade-in animation in milliseconds
+        textView.setVisibility(View.VISIBLE);
+        learnMore.setVisibility(View.GONE);
+        textView.startAnimation(fadeIn);
     }
 }
