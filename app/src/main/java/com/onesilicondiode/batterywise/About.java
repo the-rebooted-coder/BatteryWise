2cdca1d1eefe845b923f6fc53394cf894ee80787
Spandan Saxena <spandansaxena@gmail.com>
2023-09-01 02:34:54 +0530

    Developer Name Done

--- app/src/main/java/com/onesilicondiode/batterywise/About.java
+++ app/src/main/java/com/onesilicondiode/batterywise/About.java
@@ -8,9 +8,19 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.util.TypedValue;
 
+import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.color.DynamicColors;
+import com.google.android.material.shape.CornerFamily;
 
 public class About extends AppCompatActivity {
+
+    public static int getThemeColor(Context context, int colorResId) {
+        TypedValue typedValue = new TypedValue();
+        TypedArray typedArray = context.obtainStyledAttributes(typedValue.data, new int[]{colorResId});
+        int color = typedArray.getColor(0, 0);
+        typedArray.recycle();
+        return color;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +30,25 @@ public class About extends AppCompatActivity {
         getWindow().setStatusBarColor(getThemeColor(this, android.R.attr.colorPrimaryDark));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-    }
-    public static int getThemeColor(Context context, int colorResId) {
-        TypedValue typedValue = new TypedValue();
-        TypedArray typedArray = context.obtainStyledAttributes(typedValue.data, new int[]{colorResId});
-        int color = typedArray.getColor(0, 0);
-        typedArray.recycle();
-        return color;
+        MaterialCardView cardView = findViewById(R.id.initialCard);
+        float radius = 30;
+        cardView.setShapeAppearanceModel(
+                cardView.getShapeAppearanceModel()
+                        .toBuilder()
+                        .setTopLeftCorner(CornerFamily.ROUNDED, radius)
+                        .setTopRightCorner(CornerFamily.ROUNDED, radius)
+                        .setBottomRightCornerSize(0)
+                        .setBottomLeftCornerSize(0)
+                        .build());
+
+        MaterialCardView cardViews = findViewById(R.id.secondaryCard);
+        cardViews.setShapeAppearanceModel(
+                cardViews.getShapeAppearanceModel()
+                        .toBuilder()
+                        .setTopLeftCorner(CornerFamily.ROUNDED, radius)
+                        .setTopRightCorner(CornerFamily.ROUNDED, radius)
+                        .setBottomRightCornerSize(0)
+                        .setBottomLeftCornerSize(0)
+                        .build());
     }
 }
